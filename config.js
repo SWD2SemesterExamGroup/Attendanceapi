4d89cbf1ff90f5f5f9f54e9e903cb7d9159c2b49
Hidancore <nicklasandersen1@live.dk>
2018-06-01 12:52:08 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    dbconfig : {
+        host: 'mysql46.unoeuro.com',
+        port: '3306',
+        user: 'helbojensen_dk',
+        password: 'lk4ndpwE',
+        database: 'helbojensen_dk_db'
+    }
+    
+}
+/*
+host: '127.0.0.1',
+        port: '3306',
+        user: 'root',
+        database: 'Attendance_DB'*/
