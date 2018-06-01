6ee1a1d2d7ea510eab192928bd2bfcc187bbcdcf
Hidancore <nicklasandersen1@live.dk>
2018-06-01 13:18:54 +0200

    fix

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 require('./routes')(app);
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
   console.log('We are live on ' + port);
+  console.log("Process env Port: " + process.env.PORT);
 });
